fbd12db7cd5bab6b7c04b843c09c02cf0bc1de5f
SUNNY0KUMAR <125239385+SUNNY0KUMAR@users.noreply.github.com>
2025-03-01 14:49:10 +0530

    Update app.js
    complete logic changes

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector("form").addEventListener("submit", function(event) {
-        event.preventDefault(); // Prevent form from refreshing the page
+        event.preventDefault(); 
 
-        // Getting user choices
         let party = document.getElementById("party").value;
         let stalinCriticism = document.querySelector('input[name="stalin_criticism"]:checked').value;
         let occupation = document.getElementById("occupation").value;
@@ -12,16 +11,22 @@ document.addEventListener("DOMContentLoaded", function() {
         let crimes = [];
         crimeElements.forEach(crime => crimes.push(crime.value));
 
-        let resultPage = "canal.html"; 
-        if (party === "yes" && stalinCriticism === "never" && crimes.length === 0 && occupation !== "former_noble") {
+        let resultPage = "canal.html";  
+
+        if (crimes.includes("loyal_soviet") && crimes.length === 1) {
             resultPage = "safe.html";  
-        } 
+        }
         else if (party === "no" || stalinCriticism === "yes" || crimes.includes("treason") || crimes.includes("espionage")) {
             resultPage = "siberia.html"; 
-        } else if (occupation === "former_noble" || crimes.includes("religion")) {
+        }
+        else if (occupation === "former_noble" || crimes.includes("religion")) {
             resultPage = "mine.html"; 
-        } else if (rehabilitation === "refuse") {
+        }
+        else if (rehabilitation === "refuse") {
             resultPage = "execution.html";
+        }
+        else {
+            resultPage = "canal.html";
         }
 
         window.location.href = `loading.html?page=${resultPage}`;
